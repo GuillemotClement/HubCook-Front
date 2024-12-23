ac7ecdc8a15b316cbe33b50e416f16ded9574839
GuillemotClement <guillemot.clement@protonmail.com>
2024-12-23 21:29:41 +0100

    Add footer base

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,15 @@
+// import {Link} from "react-router";
+// import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+// import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 export default function Footer() {
   return (
-      <div className="">
-        <h1>Hello Footer</h1>
+      <div className="flex ustify-between py-3 px-8 border-t">
+        <p>Copyright &copy; - 2024</p>
+        {/*<ul>*/}
+        {/*  <li><Link to=""><FontAwesomeIcon icon={faGithub}/></Link></li>*/}
+        {/*  <li><Link to=""><FontAwesomeIcon icon={faLinkedin}/></Link></li>*/}
+        {/*</ul>*/}
       </div>
   )
 }
