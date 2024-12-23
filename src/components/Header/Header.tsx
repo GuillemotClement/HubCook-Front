1f12a571d7715ddf3f223cc6d9a67b98ee933b8a
GuillemotClement <guillemot.clement@protonmail.com>
2024-12-23 21:26:06 +0100

    Add Header base

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,12 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faKitchenSet} from "@fortawesome/free-solid-svg-icons";
 export default function Header() {
   return (
-      <div className="">
-        <h1>Hello Header</h1>
+      <div className="flex justify-between py-3 px-8 shadow-md items-center">
+        <div className="gap-x-2 flex items-center">
+          <FontAwesomeIcon icon={faKitchenSet} className="text-3xl"/>
+          <span className="text-xl font-semibold">HubCook</span>
+        </div>
       </div>
   )
 }
