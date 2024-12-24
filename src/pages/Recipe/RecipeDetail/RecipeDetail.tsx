a100b564b2b37fcbc5070b683b976f35833bc595
GuillemotClement <guillemot.clement@protonmail.com>
2024-12-24 23:49:26 +0100

    Fetch data on recipe detail page

--- src/pages/Recipe/RecipeDetail/RecipeDetail.tsx
+++ src/pages/Recipe/RecipeDetail/RecipeDetail.tsx
@@ -1,7 +1,28 @@
+import {useEffect, useState} from "react";
+import {useParams} from "react-router";
+
 export default function RecipeDetail() {
+
+  const { id } = useParams();
+
+  const [recipe, setRecipe] = useState({});
+
+  useEffect(() => {
+    async function fetchRecipe(){
+      const response = await fetch(`https://127.0.0.1:8000/api/recipes/${id}`)
+      const data = await response.json();
+      // console.log(data);
+      setRecipe(data);
+    }
+    fetchRecipe();
+  }, [])
+
+  console.log(recipe);
   return (
-      <div className="">
-        <h1>Hello RecipeDetail</h1>
-      </div>
+    <div className="">
+      <img src={recipe.image} alt={recipe.name}/>
+      <h1>{recipe.name}</h1>
+      <p>{recipe.describ}</p>
+    </div>
   )
 }
